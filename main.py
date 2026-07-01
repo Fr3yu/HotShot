64bec0637a3ff6365c28826504e14d49b893603b
Fr3yu <bartek.trd@gmail.com>
2026-07-01 18:10:20 +0200

    basic schedule added

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
-from storage import load_data
+import time
+import schedule
+from storage import load_data, save_data
+from scraper import scrape
 
 def sale_ended():
-    return
+    save_data()
 
 def sale_started():
-    return
+    data = scrape()
+    print(data['name'])
 
 def main():
-    print(load_data())
+    schedule.every().day.at("09:59").do(sale_ended)
+    schedule.every().day.at("10:01").do(sale_started)
+
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
 
 
 if __name__ == "__main__":
